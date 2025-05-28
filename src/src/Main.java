4bbd506d286f7676e3cf1d291fd3989bed9c191e
luucas00 <lucascamp30@gmail.com>
2025-05-27 21:11:06 -0300

    d

--- src/src/Main.java
+++ src/src/Main.java
@@ -10,6 +10,7 @@ public class Main {
 
 
 
+
         sc.close();
     }
 
---
+++ src/src/TarefaManager.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+public class TarefaManager {
+
+    //Lista de tarefas regristradas
+   private final List<Tarefa> tarefas = new ArrayList<>();
+
+   //Adicionar tarefa a lista
+   public void adicionarTarefa(Tarefa tarefa){
+       tarefas.add(tarefa);
+   }
+
+   
+
+
+
+
+
+}
