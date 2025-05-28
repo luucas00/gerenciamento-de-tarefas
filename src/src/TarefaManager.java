a022410e919b7c9229d1c463cce5bc742019acf7
luucas00 <lucascamp30@gmail.com>
2025-05-28 07:21:55 -0300

    Update TarefaManager.java

--- src/src/TarefaManager.java
+++ src/src/TarefaManager.java
@@ -8,12 +8,13 @@ public class TarefaManager {
     //Lista de tarefas regristradas
    private final List<Tarefa> tarefas = new ArrayList<>();
 
+
    //Adicionar tarefa a lista
    public void adicionarTarefa(Tarefa tarefa){
        tarefas.add(tarefa);
    }
 
-   
+
 
 
 
