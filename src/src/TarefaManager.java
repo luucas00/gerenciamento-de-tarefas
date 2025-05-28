767c8471552491f9b366c5d241bd5af150b146db
luucas00 <lucascamp30@gmail.com>
2025-05-28 15:23:29 -0300

    Built the task manager and added two ways of deleting the open tasks
    Co-Authored-By: Kawann Vinícios gadelha de Andrade  <126737729+kawanngadelha@users.noreply.github.com>

--- src/src/TarefaManager.java
+++ src/src/TarefaManager.java
@@ -14,6 +14,23 @@ public class TarefaManager {
        tarefas.add(tarefa);
    }
 
+   public boolean removerTarefasPorTitulo(String titulo){
+       return tarefas.removeIf(t -> t.getTitulo().equalsIgnoreCase(titulo));
+   }
+
+   public boolean removerTarefasPorStatus(String status){
+       return tarefas.removeIf(t -> t.getStatus().equalsIgnoreCase(status));
+   }
+
+   public void listarTarefas(){
+       if (tarefas.isEmpty()){
+           System.out.println("Nenhuma tarefa cadastrada");
+       } else {
+           for (Tarefa tarefa : tarefas){
+               System.out.println(tarefa);
+           }
+       }
+   }
 
 
 
